f5c08f23709fb6d021e05391ca7813de2eca8006
Owen Johnson <ojohnson@tcd.ie>
2023-11-21 15:55:28 +0000

    Version 2.0 w/ Examples

---
+++ data-flatten.py
@@ -0,0 +1,42 @@
+'''
+Code Purpose: 
+Code Author: Owen A. Johnson 
+Date: 2023-11-21
+'''
+#%%
+import netCDF4 as nc
+import numpy as np
+from glob import glob
+import datetime 
+from astropy.time import Time
+from tqdm import tqdm
+
+file_list = sorted(glob('input-data/*f1m*.nc'))
+print('Number of f1m files: ', len(file_list))
+print('Approximate length of final array: ', len(file_list)*1440)
+
+dates = []; densities = []; mjds = []
+
+for file in tqdm(file_list):
+    dataset = nc.Dataset(file, 'r')
+    if 'proton_density' in dataset.variables:
+        # - Density Extraction -
+        proton_density = dataset.variables['proton_density'][:]
+        densities = np.append(densities, proton_density[0:])
+
+        # - Time Conversion - 
+        time = dataset.variables['time'][:] # units: milliseconds since 1970-01-01T00:00:00Z
+        converted_time = [datetime.datetime.utcfromtimestamp(t/1000) for t in time]
+        dates = np.append(dates, converted_time)
+        time_object = Time(converted_time, scale='utc')
+        mjd = time_object.mjd; mjds = np.append(mjds, mjd)
+
+# --- Remove Negative Data Entries --- 
+print('Number of negative data entries: ', len(densities[densities < 0]))
+
+# - Write to .dat file date, mjd and density -
+data = np.column_stack((dates, mjds, densities))
+np.savetxt('DSCOVR-total-data.txt', data, fmt=['%s', '%.6f', '%.2f'], delimiter='\t', header='Date\tMJD\tDensities (cm^3)')
+print(f'Data has been merged and saved.')
+
+# %%
--- extracting-data.py
+++ extracting-data.py
@@ -7,42 +7,50 @@ Date: 2023-11-20
 import argparse
 import netCDF4 as nc
 from glob import glob
-import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator
+import pandas as pd
 import datetime 
 import numpy as np 
-import scienceplots
-plt.style.use('science')
 
-file_list = glob('input-data/*f1m*.nc')
-print('Number of f1m files: ', len(file_list))
+parser = argparse.ArgumentParser(description='Plots Solar electron densitites for a given MJD range.')
+parser.add_argument('-s', '--start', type=float, help='Start MJD', required=True)
+parser.add_argument('-f', '--end', type=float, help='End MJD', required=True)
+parser.add_argument('-p', '--plot', action='store_true', help='Plot data (default = True)', default=True)
+args = parser.parse_args()
 
-plotting = True
+df = pd.read_csv('DSCOVR-total-data.txt', delimiter='\t', header=0)
+dates = df['# Date'].values; mjds = df['MJD'].values; densities = df['Densities (cm^3)'].values
 
-for file in file_list:
-    dataset = nc.Dataset(file, 'r')
-    if 'proton_density' in dataset.variables:
-        time = dataset.variables['time'][:] # units: milliseconds since 1970-01-01T00:00:00Z
-        # convert to DD-MM-YYYY HH:MM:SS
-        converted_time = [datetime.datetime.utcfromtimestamp(t/1000) for t in time]
-        converted_time_float = [t.hour + t.minute/60 for t in converted_time]
-        proton_density = dataset.variables['proton_density'][:]
-        print('Filename: ', file)
+def nearest_idx(val, array):
+    idx = (np.abs(array - val)).argmin()
+    return idx
 
-        if plotting == True:
-            plt.figure(figsize=(16, 4), dpi = 200)
-            
-            plt.scatter(converted_time_float, proton_density, s=1, facecolors='none', edgecolors='k')
-            plt.axhline(y=proton_density.mean(), color='r', linestyle='--', label='Mean: %s' % np.round(proton_density.mean(), 2))
-            plt.axhline(y=np.median(proton_density), color='g', linestyle='--', label='Median: %s' % np.round(np.median(proton_density), 2))
-            plt.axhline(y=proton_density.mean() + proton_density.std(), color='b', linestyle=':', label='$\sigma$')
-            plt.axhline(y=proton_density.mean() - proton_density.std(), color='b', linestyle=':') 
+start_idx = nearest_idx(args.start, mjds); end_idx = nearest_idx(args.end, mjds)
+print('Start index: ', start_idx, 'End index: ', end_idx)
 
-            plt.title('Observation Start Time: ' + str(converted_time[0]))
-            plt.ylabel('Density (cm$^{-3}$)'); plt.xlabel('Time (hours)')
-            plt.xlim(0, 24); plt.legend()
-            plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
-            plt.savefig('plots/' + str(converted_time[0])[0:-9] + '.png')
-    else: 
-        print('No density variable in file: ', file)
-    break
+proton_density = densities[start_idx:end_idx]; mjd_times = mjds[start_idx:end_idx]
+# hours from MJD start time
+converted_time_float = (mjd_times - mjd_times[0])*24
+print('Number of data points: ', len(proton_density))
+
+# --- Save Values to .txt file ---
+data = np.column_stack((mjd_times, proton_density))
+np.savetxt('output-data/DSCOVR-data-%s-%s.txt' % (args.start, args.end), data, fmt=['%.6f', '%.2f'], delimiter='\t', header='MJD\tDensities (cm^3)')
+
+if args.plot == True:
+    import matplotlib.pyplot as plt
+    from matplotlib.ticker import MaxNLocator
+    import scienceplots
+
+    plt.style.use('science'); plt.figure(figsize=(16, 4), dpi = 200)
+    
+    plt.scatter(converted_time_float, proton_density, s=1, facecolors='none', edgecolors='k')
+    plt.axhline(y=proton_density.mean(), color='r', linestyle='--', label='Mean: %s' % np.round(proton_density.mean(), 2))
+    plt.axhline(y=np.median(proton_density), color='g', linestyle='--', label='Median: %s' % np.round(np.median(proton_density), 2))
+    plt.axhline(y=proton_density.mean() + proton_density.std(), color='b', linestyle=':', label='$\sigma$')
+    plt.axhline(y=proton_density.mean() - proton_density.std(), color='b', linestyle=':') 
+
+    plt.title('Observation Start Time: ' + str(dates[start_idx]))
+    plt.ylabel('Density (cm$^{-3}$)'); plt.xlabel('Time (hours)')
+    plt.legend()
+    plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.savefig('plots/DSCOVR-data-%s-%s.png' % (args.start, args.end))
